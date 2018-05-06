4735834c93edd5cabda8a4cd36903ad857843e62
Ponts <pbrink@kth.se>
2018-05-06 21:53:54 +0200

    Fixed question 9

--- ass1practical.py
+++ ass1practical.py
@@ -19,16 +19,16 @@ import random
 
 
 def calculateMean(vector):
-	sum = 0
+	summer = 0
 	for n in vector:
-		sum+=n
-	return sum/len(vector)
+		summer+=n
+	return summer/len(vector)
 
 def calculateStandardVariance(vector):
-	sum = 0
+	summer = 0
 	for n in vector:
-		sum+= n**2
-	return math.sqrt(sum/len(vector))
+		summer+= n**2
+	return math.sqrt(summer/len(vector))
 
 def plotNormalDist(mean, standVar):
 	x, y = np.mgrid[-1:1:.01, -1:1:.01]
@@ -38,7 +38,7 @@ def plotNormalDist(mean, standVar):
 	plt.contourf(x, y, rv.pdf(pos))
 	plt.show()
 
-def samplePrior(mean, standVar):
+def sampleNorm(mean, standVar):
 	return np.random.multivariate_normal(mean, standVar)
 
 def calcNewMeanStand(mean, standVar, x, t,W):
@@ -51,23 +51,38 @@ def calcTi(W,X,e):
 	return W[0]*X + W[1] + e
 def calcY(W,X):
 	return W[0]*X + W[1]
-W=np.array([1.5, -0.8])
-X=np.arange(-2.0,2.01,0.02)
+
 def sampleError():
 	return np.random.normal(0.0,0.2)
+
+
 def sampleData():
+	W=np.array([1.5, -0.8])
+	X=np.arange(-2.0,2.01,0.02)
 	x = random.choice(X)
 	return x, calcTi(W,x,sampleError())
 
+def plotFunctions(Ws, Xs, Ts):
+	plt.scatter(Xs,Ts)
+	x=np.arange(-2.0,2.01,0.02)
+	for w in Ws:
+		plt.plot(x,calcY(w,x))
+	plt.show()
+
 mean = np.array([0.0, 0.0])
 standVar=np.identity(2)
-
+Xs = []
+Ts = []
 plotNormalDist(mean, standVar)
-print(mean.shape)
+#print(mean.shape)
 for i in range(10):
 	x, t = sampleData()
-	W = samplePrior(mean, standVar)
-	print(W)
-	mean, standVar = calcNewMeanStand(mean,standVar, x, t, W)
+	Xs.append(x.item(0))
+	Ts.append(t.item(0))
+	Wnew = sampleNorm(mean, standVar)
+	Ws = np.array([sampleNorm(mean,standVar) for i in range(10)]).reshape(-1,2)
+	print(Wnew)
+	plotFunctions(Ws,Xs,Ts)
+	mean, standVar = calcNewMeanStand(mean,standVar, x, t, Wnew)
 	print(mean)
 	plotNormalDist(mean,standVar)
---
+++ newass1practical.py
@@ -0,0 +1,66 @@
+import pylab as pb
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+import random
+
+# To sample from a multivariate Gaussian
+#f = np.random.multivariate_normal(mu,K)
+# To compute a distance matrix between two sets of vectors
+#D = cdist ( x1 , x2 )
+# To compute the exponetial of all elements in a matrix
+#E = np . exp ( D )
+
+
+def plotNormalDist(mean, coVar):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    x, y = np.mgrid[-5:5:.01, -5:5:0.01]
+    pos = np.empty(x.shape + (2,))
+    pos[:, :, 0] = x; pos[:, :, 1] = y
+    rv = multivariate_normal(mean, coVar)
+    
+    plt.contourf(x, y, rv.pdf(pos))
+    plt.show()
+
+def getCoVar(X):
+    return np.linalg.inv(np.dot(X.T,X) + np.identity(2))
+
+def getMean(sigma, X,T):
+    return np.array(np.dot(sigma, np.dot(X.T,T))).reshape(-1)
+
+def calculateNewParams(x,t,sigma):
+	#newCoVar = np.linalg.inv(np.dot(x.T,x) + np.identity(2))
+	#newMean = np.array(np.dot(sigma, np.dot(x.T,t))).reshape(-1)
+	newCoVar = getCoVar(x)
+	newMean = getMean(newCoVar,x,t)
+	return newMean, newCoVar
+
+def sampleWeights(mean, covar):
+	return np.random.multivariate_normal(mean,covar)
+
+truW = [1.5, -0.8]
+
+x = np.arange(-2,2,.02)
+x = np.expand_dims(x,axis=1)
+x = np.insert(x,1,1,axis=1) 
+
+
+
+
+
+trainX = np.matrix([random.choice(x) for i in range(20)])
+trainT = (np.dot(trainX, truW) + np.random.normal(0,0.2,trainX.shape[0])).T
+
+sigma = 1
+
+newMean, newCoVar = calculateNewParams(trainX,trainT,sigma)
+plotNormalDist(newMean,newCoVar)
+
+for i in range(5):
+	W = sampleWeights(newMean,newCoVar)
+	
+	plt.plot(x[:,0],np.dot(x,W))
+plt.scatter(np.squeeze(np.asarray(trainX[:,0])),np.squeeze(np.asarray(trainT)))
+plt.show()
